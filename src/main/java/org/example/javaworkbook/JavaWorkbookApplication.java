b1309dc5b1550bd70e65c7c8a0204a8f000c2089
HY-WG <mung94c@gmail.com>
2025-08-06 09:28:01 +0900

    08.06.Quiz1

---
+++ src/main/java/org/example/javaworkbook/JavaWorkbookApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaworkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWorkbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWorkbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaworkbook/JavaWorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaworkbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWorkbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
